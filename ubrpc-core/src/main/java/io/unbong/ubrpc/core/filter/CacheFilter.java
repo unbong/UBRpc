447108277da136405a9362559d985674a1ad945c
unbong <ecunbong@gmail.com>
2024-03-24 15:58:26 +0900

    guava cache for filter

--- ubrpc-core/src/main/java/io/unbong/ubrpc/core/filter/CacheFilter.java
+++ ubrpc-core/src/main/java/io/unbong/ubrpc/core/filter/CacheFilter.java
@@ -1,11 +1,17 @@
 package io.unbong.ubrpc.core.filter;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import io.unbong.ubrpc.core.api.Filter;
 import io.unbong.ubrpc.core.api.RpcRequest;
 import io.unbong.ubrpc.core.api.RpcResponse;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 如果调用过，直接返回缓存
@@ -15,17 +21,22 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class CacheFilter implements Filter {
 
-    static Map<String,Object> cache = new ConcurrentHashMap<>();
+    Cache<String, Object> cache = CacheBuilder.newBuilder()
+            .maximumSize(40)
+            .expireAfterAccess(3, TimeUnit.MINUTES)
+            .build();
 
     @Override
     public Object preFilter(RpcRequest request) {
 
-        return cache.get(request.toString());
+        Object o = cache.getIfPresent(request.toString());
+        return o;
+
     }
 
     @Override
     public Object postFilter(RpcRequest request, RpcResponse response, Object result) {
-        cache.putIfAbsent(request.toString(), result );
+        cache.put(request.toString(), result );
 
         return result;
     }
